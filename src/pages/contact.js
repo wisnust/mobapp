acb7df272c9c6ddddeff15b391299378481f06ff
Wisnu Satrio T <wisnu21cool@gmail.com>
2018-11-23 23:13:58 +0700

    initial full commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <h1>Contact</h1>
+      </div>
+    );
+  }
+}
+
+export default contact;
